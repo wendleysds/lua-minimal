b329921464f0565c15bdc0e009f24805ca70a395
Wendley Santos <wendley.sdsilva@gmail.com>
2026-03-02 22:49:21 -0300

    feat: minimal stdio.h

---
+++ src/lua-libc/stdio.c
@@ -0,0 +1,302 @@
+#include <stdarg.h>
+#include <stdint.h>
+#include <string.h>
+
+#define ZERODAP_FLAG      0x01
+#define SHOW_SIGN_FLAG    0x02
+#define PLUS_PREFIX_FLAG  0x04
+#define SPACE_PREFIX_FLAG 0x08
+#define SPECIAL_FLAG      0x10
+#define LOWERCASE_FLAG    0x20
+#define LEFTPAD_FLAG      0x40
+
+#define __isdigit(c) ((c) >= '0' && (c) <= '9')
+#define __tolow(c) ((c) >= 'A' && (c) <= 'Z' ? (c) + 32 : (c))
+
+unsigned int do_div(unsigned long long *n, unsigned int base){
+	unsigned long long dividend = *n;
+	unsigned long long quotient = 0;
+	unsigned long long remainder = 0;
+
+	for (int i = 63; i >= 0; i--) {
+		remainder <<= 1;
+		remainder |= (dividend >> i) & 1;
+
+		if (remainder >= base) {
+			remainder -= base;
+			quotient |= (1ULL << i);
+		}
+	}
+
+	*n = quotient;
+	return (unsigned int)remainder;
+}
+
+static int skip_stoi(const char **s){
+	int i = 0;
+	while (__isdigit(**s)){
+		i = i * 10 + *((*s)++) - '0';
+	}
+		
+	return i;
+}
+
+static char* stoi(char* str, unsigned long long number, int base, int size, int precision, int flag){
+	static const char digits[] = "0123456789ABCDEF";
+
+	char tmp[128];
+	int i = 0;
+
+	if(base < 2 || base > 16){
+		return NULL;
+	}
+
+	uint8_t lowercase = (flag & LOWERCASE_FLAG);
+
+	if(flag & LEFTPAD_FLAG){
+		flag &= ~ZERODAP_FLAG;
+	}
+
+	char c = (flag & ZERODAP_FLAG) ? '0' : ' ';
+	
+	// Handle sign
+	uint8_t sign = 0;
+	if (flag & SHOW_SIGN_FLAG) {
+		if ((signed long long)number < 0) {
+			sign = '-';
+			number = -(signed long long)number;
+			size--;
+		} else if (flag & PLUS_PREFIX_FLAG) {
+			sign = '+';
+			size--;
+		} else if (flag & SPACE_PREFIX_FLAG) {
+			sign = ' ';
+			size--;
+		}
+	}
+
+	// Handle special prefix (0x, 0, 0b)
+	if(flag & SPECIAL_FLAG){
+		size -= (base == 16 || base == 2) ? 2 : (base == 8) ? 1 : 0;
+	}
+
+	// Convert number to string
+	if(number == 0){
+		tmp[i++] = '0';
+	} else {
+		if (base == 16) {
+			while (number) {
+				tmp[i++] = digits[number & 0xF];
+				number >>= 4;
+			}
+		} else if (base == 8) {
+			while (number) {
+				tmp[i++] = digits[number & 0x7];
+				number >>= 3;
+			}
+		}  else if (base == 2) {
+			while (number) {
+				tmp[i++] = digits[number & 0x1];
+				number >>= 1;
+			}
+		} else {
+			while (number) {
+				tmp[i++] = digits[do_div(&number, base)] | lowercase;
+			}
+		}
+	}
+
+	precision = (i > precision) ? i : precision;
+	size -= precision;
+
+	// Left padding with spaces
+	if(!(flag & (ZERODAP_FLAG | LEFTPAD_FLAG))){
+		while(size-- > 0) *str++ = ' ';
+	}
+
+	if(sign) *str++ = sign;
+
+	// Special prefix
+	if(flag & SPECIAL_FLAG){
+		if(base == 8) *str++ = '0';
+		else if(base == 16){
+			*str++ = '0';
+			*str++ = ('X' | lowercase);
+		}else if(base == 2){
+			*str++ = '0';
+			*str++ = 'b';
+		}
+	}
+
+	// Zero padding
+	if(!(flag & LEFTPAD_FLAG)){
+		while (size-- > 0) *str++ = c;
+	}
+
+	// Precision padding
+	while(i < precision--) *str++ = '0';
+
+	// Copy digits in reverse
+	while(i--) *str++ = tmp[i];
+
+	// Right padding
+	while(size-- > 0) *str++ = ' ';
+
+	return str;
+}
+
+int vsnprintf(char* restrict buf, unsigned int size, const char* fmt, va_list args){
+	if (size == 0) {
+		return 0; // No space to write
+	}
+
+	char *str = buf;
+	
+	for (unsigned int __i = 0; __i < size ; ++fmt, __i++) {
+		if(!*fmt){
+			break;
+		}
+
+		if (*fmt != '%') {
+			*str++ = *fmt;
+			continue;
+		}
+
+		/* parse format specifier */
+		int flags = 0;
+		while (++fmt) {
+			if (*fmt == '-') flags |= LEFTPAD_FLAG;
+			else if (*fmt == '+') flags |= PLUS_PREFIX_FLAG;
+			else if (*fmt == ' ') flags |= SPACE_PREFIX_FLAG;
+			else if (*fmt == '#') flags |= SPECIAL_FLAG;
+			else if (*fmt == '0') flags |= ZERODAP_FLAG;
+			else break;
+		}
+
+		/* field width */
+		int field_width = -1;
+		if (__isdigit(*fmt))
+			field_width = skip_stoi(&fmt);
+		else if (*fmt == '*'){
+			fmt++;
+			field_width = va_arg(args, int);
+			if (field_width < 0) {
+				field_width = -field_width;
+				flags |= LEFTPAD_FLAG;
+			}
+		}
+
+		/* precision */
+		int precision = -1;
+		if (*fmt == '.') {
+			fmt++;
+			precision = (__isdigit(*fmt)) ? skip_stoi(&fmt) : 0;
+			if (*fmt == '*'){
+				fmt++;
+				precision = va_arg(args, int);
+			}
+			if (precision < 0)
+				precision = 0;
+		}
+
+		/* qualifier */
+		int qualifier = -1;
+		if (*fmt == 'l' && *(fmt + 1) == 'l') {
+			qualifier = 'q';
+			fmt += 2;
+		} else if (*fmt == 'h' || *fmt == 'l' || *fmt == 'L'
+			|| *fmt == 'Z') {
+			qualifier = *fmt;
+			++fmt;
+		}
+
+		int base = 10;
+		switch (*fmt) {
+			case 'c':
+				if (!(flags & LEFTPAD_FLAG))
+					while (--field_width > 0) *str++ = ' ';
+				*str++ = (unsigned char)va_arg(args, int);
+				while (--field_width > 0) *str++ = ' ';
+				continue;
+			case 's': {
+					const char *s = va_arg(args, char *);
+					int len = (precision < 0) ? strlen(s) : strnlen(s, precision);
+				if (!(flags & LEFTPAD_FLAG))
+					while (len < field_width--) *str++ = ' ';
+				for (int i = 0; i < len; ++i) *str++ = *s++;
+				while (len < field_width--) *str++ = ' ';
+				continue;
+			}
+			case 'p':
+				if (field_width == -1) {
+					field_width = 2 * sizeof(void *);
+					flags |= ZERODAP_FLAG;
+				}
+				str = stoi(str, (unsigned long)va_arg(args, void *), 16,
+						field_width, precision, flags);
+				continue;
+			case 'n':
+				if (qualifier == 'l') {
+					long *ip = va_arg(args, long *);
+					*ip = (str - buf);
+				} else {
+					int *ip = va_arg(args, int *);
+					*ip = (str - buf);
+				}
+				continue;
+			case '%':
+				*str++ = '%';
+				continue;
+			case 'b': base = 2; break;
+			case 'o': base = 8; break;
+			case 'x': flags |= LOWERCASE_FLAG;
+			case 'X': base = 16; break;
+			case 'd':
+			case 'i': flags |= SHOW_SIGN_FLAG; break;
+			case 'u': break;
+			default:
+				*str++ = '%';
+				if (*fmt) *str++ = *fmt;
+				else --fmt;
+				continue;
+		}
+
+		unsigned long long num;
+
+		if (flags & SHOW_SIGN_FLAG) {
+			// signed
+			if (qualifier == 'l')
+				num = va_arg(args, long);
+			else if (qualifier == 'q')
+				num = va_arg(args, long long);
+			else if (qualifier == 'h')
+				num = (short)va_arg(args, int);
+			else
+				num = va_arg(args, int);
+		} else {
+			// unsigned
+			if (qualifier == 'l')
+				num = va_arg(args, unsigned long);
+			else if (qualifier == 'q')
+				num = va_arg(args, unsigned long long);
+			else if (qualifier == 'h')
+				num = (unsigned short)va_arg(args, unsigned int);
+			else
+				num = va_arg(args, unsigned int);
+		}
+		
+		str = stoi(str, num, base, field_width, precision, flags);
+	}
+
+	*str = '\0';
+	return str - buf;
+}
+
+int snprintf(char* restrict buf, unsigned int size, const char* restrict fmt, ...){
+	va_list args;
+	va_start(args, fmt);
+	int count = vsnprintf(buf, size, fmt, args);
+	va_end(args);
+	
+	return count;
+}
